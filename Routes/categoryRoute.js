ec4d67cb71d918e762d9942e80af33f0a909b18e
lequan <lequan2002.ql@gmail.com>
2023-04-05 10:19:31 +0700

    test merge conflic

--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -2,7 +2,7 @@ import categoryModel from "../model/categoryModel.js";
 import express from 'express'
 
 const Router = express.Router();
-
+askdagksdqwietqweugsadajks xcc
 
 export default Router;
 
