9d959f57fb41692d7d77587c660eb6b87960a592
Khoa <ldkhoa.11402@gmail.com>
2023-04-24 14:58:43 +0700

    statistic my month

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -92,6 +92,44 @@ Router.get("/statistic/day", async (req, res)=>{
   res.json(result)
 })
   
+Router.get("/statistic/month", async (req, res)=>{
+  let bookid = req.query.id
+  let month = +req.query.month-1
+  let year = +req.query.year
+  let dateMonth = getFirstAndLastDayOfMonth(year, month)
+  let result = []
+  for(let week = 1 ; week <= 4; week++){
+    let startWeek = addDay(dateMonth.start, 7*(week-1));
+    let endWeek =  addDay(startWeek, 6);
+    if(week == 4)
+      endWeek = dateMonth.end
+      let carts = await orderModel.find({
+        'DetailCart.Book._id': bookid,
+        PurchaseDate:{
+          $gte: startWeek + "T00:00:00.000Z",
+          $lte: endWeek + "T23:59:59.999Z"
+        }
+      }, 'DetailCart.QuantityBuy DetailCart.Book._id').exec()
+  
+      let totalBuyPerDay = 0
+      if(carts.length != 0){
+        for(let cart of carts){
+          for(let detailCart of cart.DetailCart){
+             if(detailCart.Book._id == bookid) {
+              totalBuyPerDay += detailCart.QuantityBuy
+             }
+          }
+        }
+      }
+      const item = {
+        category: "Week " + week,
+        quantitySelling: totalBuyPerDay
+      }
+        result.push(item)
+  }
+  res.json(result)
+})
+
 function addDay(inputDate, i) {
   let date = new Date(inputDate);
   date.setDate(date.getDate() + i);
@@ -107,5 +145,17 @@ function calculateDaysBetweenDates(dayA, dayB) {
   let timeDifference = dateB - dateA;
   let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
   return daysDifference;
+}
+function getFirstAndLastDayOfMonth(year, month) {
+  const firstDay = `${year}-${pad(month+1)}-01`;
+  const lastDay = `${year}-${pad(month+1)}-${getDaysInMonth(year,month+1)}`;
+
+  return { start:firstDay, end:lastDay };
+}
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
+function pad(d) {
+  return (d < 10) ? '0' + d.toString() : d.toString();
 }
 export default Router;
