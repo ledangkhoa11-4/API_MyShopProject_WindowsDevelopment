a2deb769adf721e2587a46ffbf23c68985f56893
lequan <lequan2002.ql@gmail.com>
2023-04-24 22:42:18 +0700

    api get profit by date

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -3,58 +3,42 @@ import orderModel from "../model/orderModel.js"
 import express from 'express'
 
 const Router = express.Router();
-Router.get('/sales', async (req, res) => {
-    try {
-      const { startDate, endDate } = req.query;
-  
-      const result = await orderModel.aggregate([
-        // Filter the orders that were purchased between the start and end dates
-        {
-          $match: {
-            PurchaseDate: {
-              $gte: new Date(startDate),
-              $lte: new Date(endDate),
-            },
-          },
-        },
-        // Unwind the DetailCart array
-        {
-          $unwind: '$DetailCart',
-        },
-        // Group the products in each order by their _id field, and calculate the total number of sales
-        {
-          $group: {
-            _id: '$DetailCart.Book._id',
-            totalSales: {
-              $sum: '$DetailCart.QuantityBuy',
-            },
-          },
-        },
-        // Lookup the product details for each group
-        {
-          $lookup: {
-            from: 'Books', // The name of the Products collection
-            localField: '_id',
-            foreignField: '_id',
-            as: 'product',
-          },
-        },
-        // Project the fields that we want to keep in the result
-        {
-          $project: {
-            _id: 1,
-            name: { $arrayElemAt: ['$product.name', 0] },
-            totalSales: 1,
-          },
-        },
-      ]);
-  
-      res.json(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+Router.get('/profit/day', async (req, res) => {
+  let startDay = req.query.start //+ "T00:00:00.000Z"
+  let endDate = req.query.end //"T23:59:59.999Z"
+  let result = []
+
+  let numday = calculateDaysBetweenDates(startDay, endDate)
+  for(let i = 0; i <=numday; i++ ){
+    let day = addDay(startDay,i)
+
+    let carts = await orderModel.find({
+      PurchaseDate:{
+        $gte: day + "T00:00:00.000Z",
+        $lte: day + "T23:59:59.999Z"
+      }
+    }, 'DetailCart.QuantityBuy DetailCart.Book._id ').exec()
+
+    let totalSelling = 0
+    let totalPurchase = 0
+    if(carts.length != 0){
+      for(let cart of carts){
+        for(let detailCart of cart.DetailCart){
+            let product = await productModel.findById({_id: detailCart.Book._id})
+            totalSelling += detailCart.QuantityBuy * product.SellingPrice
+            totalPurchase += detailCart.QuantityBuy * product.PurchasePrice
+        }
+      }
     }
-  });
+    let totalProfit = totalSelling - totalPurchase
+    const item = {
+      time: day,
+      profit: totalProfit
+    }
+      result.push(item)
+  }
+  res.json(result)
+});
 
 Router.get("/statistic/day", async (req, res)=>{
   let bookid = req.query.id
